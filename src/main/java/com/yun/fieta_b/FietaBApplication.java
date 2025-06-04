d84b6991255704c67a11b133957efabff38c343c
yun <qqeew1234@gmail.com>
2025-06-04 18:02:47 +0900

    initialize Spring Boot application with basic structure and configuration

---
+++ src/main/java/com/yun/fieta_b/FietaBApplication.java
@@ -0,0 +1,13 @@
+package com.yun.fieta_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FietaBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FietaBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yun/fieta_b/FietaBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yun.fieta_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FietaBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
