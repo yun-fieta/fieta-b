f099a70286868b8bc76595e4f2fd53d88ebd11fc
yun <qqeew1234@gmail.com>
2025-06-04 20:41:40 +0900

    update Java version to 21 and add CheckJavaVersion class for version verification

---
+++ src/main/java/com/yun/fieta_b/CheckJavaVersion.java
@@ -0,0 +1,7 @@
+package com.yun.fieta_b;
+
+public class CheckJavaVersion {
+    public static void main(String[] args) {
+        System.out.println("Running Java version: " + System.getProperty("java.version"));
+    }
+}
